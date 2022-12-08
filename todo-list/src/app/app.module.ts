f16ac6582bc44729d49df1d005fc52930a80266e
GustSilvaJR <gust.offic@gmail.com>
2022-12-08 14:29:14 -0300

    Initial commit for my new project To-Do List

---
+++ todo-list/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './mainApp/app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    RouterModule,
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
