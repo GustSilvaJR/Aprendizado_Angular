7c6123c62dd6fa3bec2dcc9d08a2687314309589
GustSilvaJR <gust.offic@gmail.com>
2022-12-12 13:57:56 -0300

    Criando meu input para adicionar novas tasks, o css para ajustá-lo adequadamente na tela. Criando função para validar quando uma task estiver toda apagada e perguntar ao usuario se deseja apagála | Criando função e atributo eventEmitter com a diretiva Output para retornar a nova task que o usuario deseja criar

--- todo-list/src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ todo-list/src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoInputAddItensComponent implements OnInit {
 
+  @Output() public emitItemTaskList:any = new EventEmitter();
+  public newItemTaskList:string = ""; 
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public submitItemTaskList():void{
+    //Retirando espaços no início e no fim
+    this.newItemTaskList = this.newItemTaskList.trim();
+    console.log(this.newItemTaskList);
+
+    if(this.newItemTaskList!=""){
+      this.emitItemTaskList.emit(this.newItemTaskList);
+      this.newItemTaskList = "";
+    }
+  }
 }
