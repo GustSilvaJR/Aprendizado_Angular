480e9a2276205b4b4c6c296b85fcac59025aa9a5
GustSilvaJR <gust.offic@gmail.com>
2022-12-13 17:03:38 -0300

    Criando requisição do tipo Post e ajustando tipo de retorno da minha requisição Get

--- myapp/src/app/services/food-list.service.ts
+++ myapp/src/app/services/food-list.service.ts
@@ -17,12 +17,16 @@ export class FoodListService {
   private url:string = "http://localhost:3000/";
 
   //Função para listar comidas que utiliza um Observable 
-  public foodList(): Observable<Food>{
-    return this.httpClient.get<Food>(this.url+'foods')
+  public foodList(): Observable<Array<Food>>{
+    return this.httpClient.get<Array<Food>>(this.url+'foods')
                 .pipe(
                   res => res,
                   error => error,
                 )
+  }
+
+  public addFood(value:Food):Observable<Food>{
+    return this.httpClient.post<Food>(this.url+'foods', { id:value.id, name:value.name, preco:value.preco })
   }
 
   //public addFood(food:string):void{
