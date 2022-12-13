bc3d3ff589e88a72b2d9cf9cf144d7af5da96b11
GustSilvaJR <gust.offic@gmail.com>
2022-12-13 16:32:01 -0300

    Criando requisição http do tipo get

--- myapp/src/app/services/food-list.service.ts
+++ myapp/src/app/services/food-list.service.ts
@@ -1,19 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Food } from '../models/food'; 
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FoodListService {
 
-  private list:Array<string> = [
-    'lanchao',
-    'barca',
-    'chocolate'
-  ];
+  constructor(private httpClient:HttpClient) { }
 
-  constructor() { }
+  //Criando meu emissor de eventos
+  public foodEvent =  new EventEmitter();
 
-  public foodList():Array<string>{
-    return this.list;
+  //Url para meu servidor
+  private url:string = "http://localhost:3000/";
+
+  //Função para listar comidas que utiliza um Observable 
+  public foodList(): Observable<Food>{
+    return this.httpClient.get<Food>(this.url+'foods')
+                .pipe(
+                  res => res,
+                  error => error,
+                )
   }
+
+  //public addFood(food:string):void{
+    //Emitindo o valor da comida adicionada
+   // this.foodEvent.emit(food);
+    
+   // this.foodList().push(food);
+  //}
 }
