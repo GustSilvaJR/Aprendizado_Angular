ab90b4bf1ebed120a62ab3122a6876079785ee79
GustSilvaJR <gust.offic@gmail.com>
2022-12-16 09:45:57 -0300

    Criação do meu componente responsável por armazenar meu template-form

---
+++ myapp/src/app/form/form.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public sendForm(form:NgForm):void{
+    alert("form enviado");
+    console.log(form);
+  }
+
+}
