fe99a0473347dcead719a7dde8fab1668a6b930d
GustSilvaJR <gust.offic@gmail.com>
2022-12-19 15:39:02 -0300

    Criação das minhas rotas padroes e criação da minha rota que implementa meu leazy load

--- myapp/src/app/app-routing.module.ts
+++ myapp/src/app/app-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './mainApp/app.component';
+import { AulasIniciaisComponent } from './aulas-iniciais/aulas-iniciais.component';
+import { HomeComponent } from './route-pages/home/home.component';
+import { PageErrorComponent } from './route-pages/page-error/page-error.component';
+import { SobreComponent } from './route-pages/sobre/sobre.component';
 
 const routes: Routes = [
-  {path:"", component:AppComponent, pathMatch:'full'}
+  {path:"", component:HomeComponent, pathMatch:"full"},
+  {path:"404", component:PageErrorComponent},
+
+  {path:"sobre", component:SobreComponent, children:[
+    {path:"sobre/:id/:name", component:SobreComponent},
+  ]},
+
+  {path:"dashboard", loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)},
+
+  {path:"aulasIniciais", component:AulasIniciaisComponent},
+  {path:"**", redirectTo:'404'}
 ];
 
 @NgModule({
