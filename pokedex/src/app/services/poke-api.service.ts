4fec94ac7c99364f598af3ce7580e36ce30dfa24
GustSilvaJR <gust.offic@gmail.com>
2022-12-20 16:51:11 -0300

    Criando minha requisição para recuperar todos os pokemons e mais uma função para consultar os status de um pokemon com base em sua url específica

--- pokedex/src/app/services/poke-api.service.ts
+++ pokedex/src/app/services/poke-api.service.ts
@@ -1,9 +1,50 @@
+
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+//Observable
+import { Observable, tap } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeApiService {
 
-  constructor() { }
+  private url: string = "https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20"
+
+  constructor(private httpClient:HttpClient) { }
+
+
+  public getAllPokemons():Observable<any>{
+    return this.httpClient.get(this.url).pipe(
+      res => {
+        console.log(res);
+        return res;
+      },
+      error => error
+    );
+  };
+
+  public getStatusPokemon(url:string):Observable<any>{
+    return this.httpClient.get(url).pipe(
+      res => {
+        return res;
+      },
+      error => error
+    );
+  };
+
+
+  // get getAllPokemons():Observable<any>{
+  //   return this.httpClient.get<any>(this.url).pipe(
+  //     tap(
+  //       res => res
+  //     ),
+  //     tap( res => {
+  //       console.log(res)
+  //     })
+  //   )
+  // }
+
 }
