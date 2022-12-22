7f6b4bb61c28a54288c732a7e85c9411bbf7d218
GustSilvaJR <gust.offic@gmail.com>
2022-12-22 12:13:43 -0300

    Criando atributo e função para que possa captar o valor pesquisado pelo usuario assim que for emitido pelo componente poke-search

--- pokedex/src/app/pages/home/home.component.ts
+++ pokedex/src/app/pages/home/home.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public searchData:string = "";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public catchSearchValue(value:string){
+    this.searchData = value;
+    console.log(`Captei: ${value}`);
   }
 
 }
