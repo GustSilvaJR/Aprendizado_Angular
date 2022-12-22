7c96bd634a8655d60a225afb8d4e7b361ab67cf6
GustSilvaJR <gust.offic@gmail.com>
2022-12-22 12:08:48 -0300

    Captando input passado pelo componente pai, e com base nisso criando função que filtra a lista de pokemons na tela | Criando trativa de erro para caso o pokeApi apresente alguma falha | Implementando estrategia para que se possa resetar a lista de pokemons quando o usuario apagar o que estava pesquisando

--- pokedex/src/app/shared/poke-list/poke-list.component.ts
+++ pokedex/src/app/shared/poke-list/poke-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Pokemon } from 'src/app/models/pokemon';
 
 //Services
@@ -9,9 +9,13 @@ import { PokeApiService } from 'src/app/services/poke-api.service';
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
-export class PokeListComponent implements OnInit {
+export class PokeListComponent implements OnInit, OnChanges {
 
-  public listPokemons:Array<Pokemon> = [];
+  @Input() public searchPokemon: string = "";
+
+  public listFixedPokemons: Array<Pokemon> = [];
+  public listPokemons: Array<Pokemon> = [];
+  public apiError: Boolean = false;
 
   constructor(private pokeApiService: PokeApiService) { }
 
@@ -19,18 +23,43 @@ export class PokeListComponent implements OnInit {
 
     this.pokeApiService.getAllPokemons().subscribe({
       next: (res) => {
-        res.results.forEach((pokemon:any) => { 
+        res.results.forEach((pokemon: any) => {
           this.pokeApiService.getStatusPokemon(pokemon.url).subscribe({
-            next: (res) =>{
+            next: (res) => {
+              res.types.forEach((e: any) => {
+                e.type.name = e.type.name[0].toUpperCase() + e.type.name.substring(1);
+              });
               pokemon.status = res;
-              console.log(pokemon);
+              pokemon.name = pokemon.name[0].toUpperCase() + pokemon.name.substring(1);
+              this.listPokemons.push(pokemon);
             },
             error: (err) => err
           });
         });
+        this.listFixedPokemons = this.listPokemons;
       },
-      error: (err) => console.log(err)
+      error: (err) => {
+        this.apiError=true;
+      }
     })
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+    if (this.searchPokemon === "") {
+      this.listPokemons = this.listFixedPokemons;
+    } else {
+      const filter = this.listFixedPokemons.filter((res: any) => {
+        this.searchPokemon = this.searchPokemon.toLowerCase();
+        this.searchPokemon = this.searchPokemon[0].toUpperCase() + this.searchPokemon.substring(1);
+        return !res.name.indexOf(this.searchPokemon)
+      });
+
+      this.listPokemons = filter;
+      console.log(filter);
+      console.log(`Mudança: ${changes} | ${this.searchPokemon}`);
+    }
+
   }
 
 }
