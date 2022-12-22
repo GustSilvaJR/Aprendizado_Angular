632472233158da4b03df856ab1a087a84fa270f8
GustSilvaJR <gust.offic@gmail.com>
2022-12-22 12:12:12 -0300

    Ajustando link base para que fique de forma dinamica no uso de todas as funções | Criando função para consultar pokemon pelo Id e criando função para consultar a especie do pokemon com base no id

--- pokedex/src/app/services/poke-api.service.ts
+++ pokedex/src/app/services/poke-api.service.ts
@@ -11,13 +11,13 @@ import { Observable, tap } from 'rxjs';
 })
 export class PokeApiService {
 
-  private url: string = "https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20"
+  private url: string = "https://pokeapi.co/api/v2/pokemon"
 
   constructor(private httpClient:HttpClient) { }
 
 
   public getAllPokemons():Observable<any>{
-    return this.httpClient.get(this.url).pipe(
+    return this.httpClient.get(`${this.url}/?limit=100`).pipe(
       res => {
         console.log(res);
         return res;
@@ -25,6 +25,20 @@ export class PokeApiService {
       error => error
     );
   };
+
+  public getPokemonById(id:string):Observable<any>{
+    return this.httpClient.get<any>(`${this.url}/${id}`).pipe(
+      res => res,
+      error => error
+    );
+  };
+
+  public getSpeciePokemonById(id:string):Observable<any>{
+    return this.httpClient.get<any>(`${this.url}-species/${id}`).pipe(
+      res => res,
+      err => err
+    )
+  }
 
   public getStatusPokemon(url:string):Observable<any>{
     return this.httpClient.get(url).pipe(
