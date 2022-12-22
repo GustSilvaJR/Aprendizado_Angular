c5ffb834696a6b449f5ca45fbfbcdd7c530684ac
GustSilvaJR <gust.offic@gmail.com>
2022-12-22 12:16:31 -0300

    Consumindo minhas apis de consulta de pokemon por id e consulta de especie do pokemon por id

--- pokedex/src/app/pages/details/details.component.ts
+++ pokedex/src/app/pages/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { PokeApiService } from 'src/app/services/poke-api.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  public pokemon: any;
+  public isLoading:Boolean = false;
+  public apiError:Boolean = false;
+
+  constructor(private pokeApiService: PokeApiService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getInfoPokemon();
+  }
+
+  public getInfoPokemon() {
+    var idPokemon = this.activatedRoute.snapshot.params['id'];
+    this.pokeApiService.getPokemonById(idPokemon).subscribe({
+      next: (res) => {
+        this.pokemon = res;
+        this.pokeApiService.getSpeciePokemonById(idPokemon).subscribe({
+          next: (resEsp: any) => {
+            this.pokemon.japName = resEsp.names[0].name;
+            this.isLoading = true;
+          },
+          error: (err: Error) => err
+        })
+      },
+      error: (err) => {
+        this.apiError = true;
+      }
+    })
   }
 
 }
