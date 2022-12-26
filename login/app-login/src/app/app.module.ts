eaf7d578ba7f00fb800004c9cd54e8a944a9f910
GustSilvaJR <gust.offic@gmail.com>
2022-12-26 17:09:35 -0300

    Adicionando modulo para fazer requisições e modulo para trabalhar com reactive forms

--- login/app-login/src/app/app.module.ts
+++ login/app-login/src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './mainApp/app.component';
@@ -12,7 +14,9 @@ import { AppComponent } from './mainApp/app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
