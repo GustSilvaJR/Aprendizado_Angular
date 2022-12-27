e47dc7544e87b13b5a208c74e423927693e7de9f
GustSilvaJR <gust.offic@gmail.com>
2022-12-27 11:48:25 -0300

    Atualizando função responsavel por fazer o sigin do usuario, salvando o token que recebe do back end no local storage durante o login do usuario | Criando serviço para efetuar o logout do usuario, retirando o token do localstorage | Criando uma função para autenticar se o token do usuario está expirado ou nao

--- login/app-login/src/app/core/components/auth/services/auth.service.ts
+++ login/app-login/src/app/core/components/auth/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +11,37 @@ export class AuthService {
 
   private url: string = 'http://localhost:3000';
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient: HttpClient, private router:Router, private jwt:JwtHelperService) { }
 
-  public sign(payload:{email:string | null | undefined, password:string | null | undefined}):Observable<any>{
+  public sign(payload: { email: string | null | undefined, password: string | null | undefined }): Observable<any> {
     return this.httpClient.post<any>(`${this.url}/sign`, payload).pipe(
-      res => res,
-      err => err
-    )
+      map((res) => {
+        console.log(res.token);
+        localStorage.removeItem('access_token');
+        localStorage.setItem('access_token', res.token);
+        return this.router.navigate(['admin']);
+      }),
+
+      catchError((e) => {
+        if (e.error.message) throw new Error(e.error.message);
+        throw new Error("Servidor fora do ar no momento");
+
+      })
+    );
+  };
+
+  public logout(){
+    localStorage.removeItem('access_token');
+    return this.router.navigate(['']);
+  }
+
+  public isAuthenticated():boolean{
+    const token = localStorage.getItem('access_token');
+    
+    if(!token) return false;
+
+    return !this.jwt.isTokenExpired(token);
+    
   }
 }
 
