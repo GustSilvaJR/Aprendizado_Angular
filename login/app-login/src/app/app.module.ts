3a4528bbf75cc839e858a421464c05b5df6201b1
GustSilvaJR <gust.offic@gmail.com>
2022-12-27 11:39:53 -0300

    Importando jwt Helper

--- login/app-login/src/app/app.module.ts
+++ login/app-login/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {ReactiveFormsModule} from '@angular/forms';
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './mainApp/app.component';
@@ -16,7 +17,12 @@ import { AppComponent } from './mainApp/app.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    JwtModule.forRoot({
+      config:{
+        allowedDomains:["http://localhost:3000"]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
