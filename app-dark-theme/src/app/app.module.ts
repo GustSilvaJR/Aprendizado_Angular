efe4d1767a26377eb0acd2b2b8bdd3af046ee486
GustSilvaJR <gust.offic@gmail.com>
2022-12-27 16:00:33 -0300

    Declarando novos componentes criados

--- app-dark-theme/src/app/app.module.ts
+++ app-dark-theme/src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './mainApp/app.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { SobreComponent } from './core/pages/components/sobre/sobre.component';
+import { HomeComponent } from './core/pages/components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SobreComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
