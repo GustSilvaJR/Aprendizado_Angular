1b3aca4459e8567f9203424888f1415ac06386db
GustSilvaJR <gust.offic@gmail.com>
2023-01-05 10:08:48 -0300

    Finalizando testes unitarios | Testes de requisição usando mock

--- testes/src/app/shared/investiments/list/list.component.spec.ts
+++ testes/src/app/shared/investiments/list/list.component.spec.ts
@@ -2,10 +2,33 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'; 
 
 import { ListComponent } from './list.component';
+import { Investiments } from '../../models/investiments';
+import { ListInvestimentsService } from '../services/list-investiments.service';
+import { of } from 'rxjs';
 
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
+  let service: ListInvestimentsService;
+
+  let mock:Array<Investiments> = [
+    {
+      name: 'Banco teste 1',
+      value: 40
+    },
+    {
+      name: 'Banco teste 2',
+      value: 50
+    },
+    {
+      name: 'Banco teste 3',
+      value: 60
+    },
+    {
+      name: 'Banco teste 4',
+      value: 70
+    },
+  ]
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,6 +38,7 @@ describe('ListComponent', () => {
     .compileComponents();
 
     fixture = TestBed.createComponent(ListComponent);
+    service = TestBed.inject(ListInvestimentsService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -22,5 +46,15 @@ describe('ListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('U Will list all the investiments'), ()=>{
+    let investiments = component.investimentos;
+    spyOn(service, 'list').and.returnValue(of(mock)); //Realizando o mock 
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    expect(service.list).toHaveBeenCalledWith();
+  }
   
 });
--- testes/src/app/shared/investiments/services/list-investiments.service.spec.ts
+++ testes/src/app/shared/investiments/services/list-investiments.service.spec.ts
@@ -3,11 +3,33 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 
 import { ListInvestimentsService } from './list-investiments.service';
 import { HttpClient } from '@angular/common/http';
+import { Investiments } from '../../models/investiments';
 
 describe('ListInvestimentsService', () => {
   let service: ListInvestimentsService;
   let httpTestingController: HttpTestingController;
   let httpClient: HttpClient;
+
+  let URL:string = 'https://raw.githubusercontent.com/troquatte/fake-server/main/investiments-all.json';
+
+  let mock:Array<Investiments> = [
+    {
+      name: 'Banco teste 1',
+      value: 40
+    },
+    {
+      name: 'Banco teste 2',
+      value: 50
+    },
+    {
+      name: 'Banco teste 3',
+      value: 60
+    },
+    {
+      name: 'Banco teste 4',
+      value: 70
+    },
+  ]
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -22,4 +44,16 @@ describe('ListInvestimentsService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('(U) should list all investiments', (done) =>{
+    service.list().subscribe((res: Array<Investiments>) => {
+      expect(res[0].name).toEqual('Banco teste 1');
+      done();
+    });
+  
+    const req = httpTestingController.expectOne(URL); //Testo qual a url eu vou utilizar
+    req.flush(mock);
+
+    expect(req.request.method).toEqual('GET'); 
+  })
 });
