44b12166819b6d01982beb65aba0bd13fb525698
Gustavo <gust.offic@gmail.com>
2024-01-18 09:02:23 -0300

    Finalizano filtros utilizando observer | Implementando debounceTime para controlar filtro por pesquisa | Implementando take para desinscrever usuario apos ele ter finalizado uma requisição especifica | Iniciando a criação de uma variavel para armazenar minhas inscrições do componente

--- Rx_Js_Angular_Course/ProjectSchool/src/app/pages/courses/courses-list/courses-list.component.ts
+++ Rx_Js_Angular_Course/ProjectSchool/src/app/pages/courses/courses-list/courses-list.component.ts
@@ -1,47 +1,99 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { PageEvent } from '@angular/material/paginator';
 import { CoursesService } from '@app/services/courses.service';
 import { Category, Course } from '@app/shared/models/course';
+import { Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-courses-list',
   templateUrl: './courses-list.component.html',
-  styleUrls: ['./courses-list.component.scss']
+  styleUrls: ['./courses-list.component.scss'],
 })
-export class CoursesListComponent implements OnInit{
-  public courseList:Course[] = [];
-  public categoryValue = Object.values(Category)
+export class CoursesListComponent implements OnInit OnDestroy {
+  public courseList: Course[] = [];
+  public categoryValue = Object.values(Category);
   public form!: FormGroup;
   private courseService = inject(CoursesService);
   private fb = inject(FormBuilder);
+  private sub!: Subscription;
 
+  public totalCount: number = 0;
+  public currentPage: number = 1;
+  public pageSize: number = 5;
 
-  constructor(){}
+  constructor() {}
 
   ngOnInit(): void {
-    this.getCourses();
     this.validation();
+    this.form.valueChanges.subscribe((value) => {
+      debounceTime(1000);
+      if (value) {
+        this.getCourses(
+          this.currentPage,
+          this.pageSize,
+          this.f.category.value ?? '',
+          this.f.search.value ?? ''
+        );
+      }
+    });
+    this.getCourses(1, 5, '', '');
   }
 
-  private validation(): void{
+  ngOnDestroy(): void {
+      this.sub.unsubscribe();
+  }
+
+  private validation(): void {
     this.form = this.fb.group({
       category: [''],
       search: [''],
-    })
+    });
   }
 
-  public doSearch(): void{
-
+  public doSearch(): void {
+    this.getCourses(
+      this.currentPage,
+      this.pageSize,
+      this.f.category.value ?? '',
+      this.f.search.value ?? ''
+    );
   }
 
-  public getCourses(): void {
-    this.courseService.getCourses().subscribe({
-      next: (response: Course[]) =>{
-        this.courseList = response;
-      },
-      error: (response:any) =>{
-        console.log("Erro ao consumir API dos cursos");
-      }
-    })
+  public get f(): any {
+    return this.form.controls;
+  }
+
+  public getCourses(
+    currentPage: number,
+    pageSize: number,
+    category: string,
+    search: string
+  ): void {
+    this.sub = this.courseService
+      .getCourses(currentPage, pageSize, category, search)
+      .subscribe({
+        next: (response: HttpResponse<any>) => {
+          this.courseList = response.body as Course[];
+          let totalCount = response.headers.get('X-Total-Count');
+          this.totalCount = totalCount ? Number(totalCount) : 0;
+        },
+
+        error: (response: any) => {
+          console.log('Erro ao consumir API dos cursos');
+        },
+      });
+  }
+
+  public handlePageEvent(e: PageEvent): void {
+    this.currentPage = e.pageIndex + 1;
+    this.pageSize = e.pageSize;
+    this.getCourses(
+      this.currentPage,
+      this.pageSize,
+      this.f.category.value ?? '',
+      this.f.search.value ?? ''
+    );
   }
 }
--- Rx_Js_Angular_Course/ProjectSchool/src/app/services/courses.service.ts
+++ Rx_Js_Angular_Course/ProjectSchool/src/app/services/courses.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Course } from '@app/shared/models/course';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -9,30 +9,47 @@ import { environment } from 'src/environments/environment';
 })
 export class CoursesService {
 
-  baseURL = `${environment.apiURL}/courses `;
+  baseURL = `${environment.apiURL}/courses`;
+
+
+
   private http = inject(HttpClient);
 
   constructor() {
   }
 
-  public getCourses(): Observable<Course[]> {
-    return this.http.get<Course[]>(`${this.baseURL}`)
+  public getCourses(currentPage:number, pageSize:number, category:string, search:string): Observable<HttpResponse<any>> {
+    console.log('Aqui', environment.apiURL);
+    let url = `${this.baseURL}?_page=${currentPage}&_limit=${pageSize} `;
+    console.log('Aqui', url);
+
+    if(search)
+      url = `${url}&q=${search}`;
+
+    console.log('Aqui', url);
+
+    if(category)
+      url = `${url}&category=${category}`;
+
+    console.log('Aqui', url);
+
+    return this.http.get<Course[]>(`${url}`, {observe: 'response'}).pipe(take(1));
   }
 
   public getCourseById(id: number): Observable<Course[]> {
-    return this.http.get<Course[]>(`${this.baseURL}/${id}`)
+    return this.http.get<Course[]>(`${this.baseURL}/${id}`).pipe(take(1));
   }
 
   public createCourse(course: Course): Observable<Course[]> {
-    return this.http.post<Course[]>(`${this.baseURL}`, course)
+    return this.http.post<Course[]>(`${this.baseURL}`, course).pipe(take(1));
   }
 
   public changeCourse(id: number, course:Course): Observable<Course[]> {
-    return this.http.put<Course[]>(`${this.baseURL}/${id}`, course)
+    return this.http.put<Course[]>(`${this.baseURL}/${id}`, course).pipe(take(1));
   }
 
   public deleteCourse(id: number): Observable<Course[]> {
-    return this.http.delete<Course[]>(`${this.baseURL}/${id}`)
+    return this.http.delete<Course[]>(`${this.baseURL}/${id}`).pipe(take(1));
   }
 
 
