d48af9ee9909918ae7480b3ce023a216aefb020a
Gustavo <gust.offic@gmail.com>
2024-01-23 10:50:18 -0300

    Finalizando atualizacao do metodo subscribe para uso com pipe async e implementando tratamento de erros

--- Rx_Js_Angular_Course/ProjectSchool/src/app/services/courses.service.ts
+++ Rx_Js_Angular_Course/ProjectSchool/src/app/services/courses.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Course } from '@app/shared/models/course';
-import { Observable, take } from 'rxjs';
+import { Observable, catchError, take, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -21,35 +21,67 @@ export class CoursesService {
   public getCourses(currentPage:number, pageSize:number, category:string, search:string): Observable<HttpResponse<any>> {
     console.log('Aqui', environment.apiURL);
     let url = `${this.baseURL}?_page=${currentPage}&_limit=${pageSize} `;
-    console.log('Aqui', url);
 
     if(search)
       url = `${url}&q=${search}`;
 
-    console.log('Aqui', url);
-
     if(category)
       url = `${url}&category=${category}`;
 
-    console.log('Aqui', url);
-
-    return this.http.get<Course[]>(`${url}`, {observe: 'response'}).pipe(take(1));
+    //Take para se desiscrever assim que a requisição finalizar
+    return this.http.get<Course[]>(`${url}`, {observe: 'response'})
+    .pipe(
+      take(1),
+      catchError(this.handlerError)
+      );
   }
 
   public getCourseById(id: number): Observable<Course[]> {
-    return this.http.get<Course[]>(`${this.baseURL}/${id}`).pipe(take(1));
+
+    return this.http.get<Course[]>(`${this.baseURL}/${id}`)
+    .pipe(
+      take(1),
+      catchError(this.handlerError)
+      );
   }
 
   public createCourse(course: Course): Observable<Course[]> {
-    return this.http.post<Course[]>(`${this.baseURL}`, course).pipe(take(1));
+
+    return this.http.post<Course[]>(`${this.baseURL}`, course)
+    .pipe(
+      take(1),
+      catchError(this.handlerError)
+      );
   }
 
   public changeCourse(id: number, course:Course): Observable<Course[]> {
-    return this.http.put<Course[]>(`${this.baseURL}/${id}`, course).pipe(take(1));
+
+    return this.http.put<Course[]>(`${this.baseURL}/${id}`, course)
+    .pipe(
+      take(1),
+      catchError(this.handlerError)
+      );
   }
 
   public deleteCourse(id: number): Observable<Course[]> {
-    return this.http.delete<Course[]>(`${this.baseURL}/${id}`).pipe(take(1));
+
+    return this.http.delete<Course[]>(`${this.baseURL}/${id}`)
+    .pipe(
+      take(1),
+      catchError(this.handlerError)
+      );
+  }
+
+  private handlerError(err: HttpErrorResponse): Observable<never> {
+
+    let errorMessage: string;
+    if(err.error instanceof ErrorEvent){
+      errorMessage = `An error ocurred: ${err.error.message}`;
+    }else{
+      errorMessage = `Backend returned code: ${err.status}: ${err.message}`;
+    }
+    console.error(err);
+    return throwError(() => errorMessage);
   }
 
 
